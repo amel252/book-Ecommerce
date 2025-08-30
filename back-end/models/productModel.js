32bee0b66fdf2ad88fcec5e53f67b450ca3b93d7
Amel252 <amelghanem25@gmail.com>
2025-08-30 15:43:41 +0200

    installation de mongoose et création de productModel

---
+++ back-end/models/productModel.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+// model pour les avis :
+const reviewsSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User", // fait refernce au model user
+        },
+        // on aura besoin de l'Id du user + son Nom
+        name: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+// model pour le produit
+const productSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User", // fait refernce au model user
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        // contenu de l'avis
+        reviews: [reviewsSchema],
+        // note d'évaluation
+        rating: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        // nombre d'avis
+        numberReviews: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+const Product = mongoose.model("Product", productSchema);
+export default Product;
