70eb775eb2e01b65b1e021b98131fc0293928100
Amel252 <amelghanem25@gmail.com>
2025-09-03 14:58:44 +0200

    Création Crud Admin (admin qui va gerer )

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -79,7 +79,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Déconnexion reussi" });
 });
 
-// get user profile (private)
+// --------------get user profile (private)-------------------
 
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
@@ -145,3 +145,89 @@ const updateUserProfile = asyncHandler(async (req, res) => {
         throw new Error("utilisateur non trouvé ");
     }
 });
+
+// ------------get all users private pour l'admin -----
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({}); // Récupère tous les documents de la collection User
+    res.status(200).json(users); // Renvoie les résultats en JSON avec le code 200
+});
+
+// ------------------l'Admin va delete user---------
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    // si n'existe pas
+    if (!user) {
+        res.status(400);
+        throw new Error("L'utilisateur non trouvé");
+    }
+    // si est admin
+    if (user.isAdmin) {
+        res.status(400);
+        throw new Error(
+            "Vous ne pouvez pas supprimer un compte administrateur"
+        );
+    }
+    // si user existe
+    await User.deleteOne({ _id: user._id });
+    res.status(200).json({ message: "Utlisateur supprimé " });
+});
+// ---l'admin va récuperer l'user a traver son id (private admin )--
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params._id).select("-password");
+    // si existe
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(400);
+        throw new Error("L'utilisateur non trouvé");
+    }
+});
+// ----L'admin va mettre a jour un user --
+
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params._id);
+    // si n'existe pas
+    if (!user) {
+        res.status(400);
+        throw new Error("L'utilisateur non trouvé");
+    }
+    // si email existe et n'est pas valide
+    if (req.body.email && !validator.isEmail(req.body.email)) {
+        res.status(400);
+        throw new Error("L'adresse email invalide");
+    }
+    // empecher l'admin a devenir user simple
+    if (user.isAdmin && req.body.isAdmin === false) {
+        res.status(400);
+        throw new Error("vous ne pouvez pas changer le role Admin ");
+    }
+    // mise a jour des champs user
+    // si la mise a jour est défini on l'a prend sinon on garde la valeur précédente
+    user.name = req.body.name ?? user.name;
+    user.email = req.body.email ?? user.email;
+
+    // mise a jour de isAdmin seuelemnt si fourni (si autorisé )
+    // l'admin peut modifié  le role de l'user , si le type est défirent qu'une chaine de caractere
+    if (typeof req.body.isAdmin !== "undefined") {
+        user.isAdmin = Boolean(req.body.isAdmin);
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+        isAdmin: updateUser.isAdmin,
+    });
+});
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    updateUser,
+    getUserById,
+};
