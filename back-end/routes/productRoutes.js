af36750eb9eff562f49b55af8eb0716801cdae9f
Amel252 <amelghanem25@gmail.com>
2025-09-03 15:42:23 +0200

    Protection des userRoutes, ProductRoute a travers les middleware

--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -11,13 +11,16 @@ import {
     createProductReview,
 } from "../controller/productController.js";
 
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 // sur la meme route je récupére et je crée ,
-router.route("/").get(getProducts).post(createProduct);
+// --- sur cette route on protége la route de création des produits (faite que par l'admin )
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 // d'habiture on sépare les routes ('/updateroute' , "/deleteproduct"), pour ne pas répéter plusierus lignes
 router
     .route("/:id")
     .get(getProductById)
-    .put(updateProduct)
-    .delete(deleteProduct);
-router.route("/:id/reviews").post(createProductReview);
+    .put(protect, admin, updateProduct)
+    .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 export default router;
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -12,10 +12,19 @@ import {
     updateUser,
     getUserById,
 } from "../controller/userController.js";
-router.route("/").post(registerUser).get(getUsers);
+import { protect, admin } from "../middleware/authMiddleware.js";
+// je ne protege la page d'inscription est visieul par l'user
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("logout", logoutUser);
 router.post("/auth", authUser);
-router.route("/profil").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+router
+    .route("/profil")
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+router
+    .route("/:id")
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 
 export default router;
