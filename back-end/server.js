1c79307d3ead8d5d318172289c2c94ea4d5c8027
Amel252 <amelghanem25@gmail.com>
2025-09-03 15:15:11 +0200

    Création des routes  ou l'user est Admin

--- back-end/server.js
+++ back-end/server.js
@@ -21,6 +21,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 const port = process.env.PORT || 8081;
@@ -28,6 +29,7 @@ connectDB();
 
 // routes
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Execute"));
 
