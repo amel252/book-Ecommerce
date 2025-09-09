cf4bf53804b95d6def75fa6bf92cdc1a2319e0a1
Amel252 <amelghanem25@gmail.com>
2025-09-09 14:58:08 +0200

    Creation de productApiSlice

---
+++ front-end/src/slices/produitApiSlice.js
@@ -0,0 +1,61 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: ({ keyword, pageNumber }) => ({
+                url: PRODUCTS_URL,
+                params: {
+                    keyword,
+                    pageNumber,
+                },
+            }),
+            providesTags: ["Product"],
+            keepUnusedDataFor: 5,
+        }),
+        getProductDetails: builder.query({
+            query: ({ productId }) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        createProduct: builder.mutation({
+            query: () => ({
+                url: { PRODUCTS_URL },
+                method: "POST",
+            }),
+            invalidatesTags: [product],
+        }),
+        updateProduct: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}`,
+                method: "PUT",
+            }),
+            invalidatesTags: [product],
+        }),
+        deleteProduct: builder.mutation({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${data.productId}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: [product],
+        }),
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: "POST",
+            }),
+            invalidatesTags: [product],
+        }),
+    }),
+});
+
+export const {
+    useGetProductsQuery,
+    useCreateProductMutation,
+    useGetProductDetailsQuery,
+    useUpdateProductMutation,
+    useDeleteeProductMutation,
+    useCreateReviewMutation,
+} = productApiSlice;
