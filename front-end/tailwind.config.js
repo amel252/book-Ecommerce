d7453c32ff24cb6fcbc3127aaa583196bb93b336
Amel252 <amelghanem25@gmail.com>
2025-09-09 16:26:15 +0200

    Installation de tailwind sur notre projet front + mise a jour header partie 2

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1d4ed8",
+                secondary: "#9333ea",
+            },
+        },
+    },
+    plugins: [],
+};
