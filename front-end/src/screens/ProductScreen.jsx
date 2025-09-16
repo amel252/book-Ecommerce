bb1a983d83fdbeac2d05dd7b73d945aec6957944
Amel252 <amelghanem25@gmail.com>
2025-09-16 15:36:29 +0200

    ProductScreen partie 2

--- front-end/src/screens/ProductScreen.jsx
+++ front-end/src/screens/ProductScreen.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
     useCreateReviewMutation,
     useGetProductDetailsQuery,
-} from "../slices/produitApiSlice";
+} from "../slices/productApiSlice";
 import Loader from "../components/Loader";
 import Rating from "../components/Rating";
 import Message from "../components/Message";
@@ -14,7 +14,6 @@ import { FaWindowClose } from "react-icons/fa";
 
 function ProductScreen() {
     const [isOpen, setIsOpen] = useState(false);
-    // je précise productId pour faire pareil comme déclaré sur ProductApiSlice
     const { id: productId } = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -30,23 +29,92 @@ function ProductScreen() {
         error,
     } = useGetProductDetailsQuery(productId);
     console.log(product);
-
-    // pour récuperer les infos sur local stockage au moment de la connexion
     const { userInfo } = useSelector((state) => state.auth);
+
     const [createReview, { isLoading: loadingProductReview }] =
         useCreateReviewMutation();
 
     const submitHandler = async (e) => {
         e.preventDefault();
+
         try {
             await createReview({
                 productId,
                 rating,
                 comment,
             }).unwrap();
-            toast.success("Avis crée avec succès");
+            toast.success("Avis créé avec succès");
         } catch (error) {}
     };
-    return <div></div>;
+
+    return (
+        <div className="max-w-6xl mx-auto p-6">
+            <Link to="/">
+                <IoChevronBackCircleSharp
+                    size={35}
+                    className="text-primary hover:text-secondary"
+                />
+            </Link>
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">
+                    {error?.data?.message || error.error}
+                </Message>
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+                    <div className="lg:col-span-1">
+                        <img
+                            src={product.image}
+                            alt={product.name}
+                            className="w-full rounded-lg shadow-lg"
+                        />
+                    </div>
+                    <div className="lg:col-span-1">
+                        <h1 className="text-3xl font-bold text-gray-900 border-b border-gray-300 pb-4">
+                            {product.name}
+                        </h1>
+                    </div>
+                    <div className="mt-4 border-b border-gray-300 pb-4">
+                        <Rating
+                            value={product.rating}
+                            text={`${product.numberReviews} Reviews`}
+                        />
+                    </div>
+                    <p className="mt-4 text-gray-700">{product.description}</p>
+                    <div className="lg:col-span-1">
+                        <div className="bg-white p-6 rounded-lg shadow-md">
+                            <div className="space-y-4">
+                                <p className="flex justify-between text-lg">
+                                    <span className="text-gray-600">
+                                        Price:
+                                    </span>
+                                    <span className="font-semibold">
+                                        €{product.price}
+                                    </span>
+                                </p>
+                                <p className="flex justify-between text-lg">
+                                    <span className="text-gray-600">
+                                        Status
+                                    </span>
+                                    <span
+                                        className={`${
+                                            product.countInStock > 0
+                                                ? "text-green-600"
+                                                : "text-red-600"
+                                        }`}
+                                    >
+                                        {product.countInStock > 0
+                                            ? "In Stock"
+                                            : "Out of stock"}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 export default ProductScreen;
